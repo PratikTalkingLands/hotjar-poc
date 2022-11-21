39c118018c4d98617a552a2b609cbb1a3fee7fce
Pratik Chakraborty <pratik@talkinglands.com>
2022-11-21 16:04:31 +0530

    google analytics event added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { hotjar } from 'react-hotjar';
 import ReactGA from 'react-ga'
@@ -12,8 +12,25 @@ ReactGA.initialize(TRACKING_ID)
 
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const addName = (e) => {
+    setName(e.target.value)
+  }
+  const addEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const addPassword = (e) => {
+    setPassword(e.target.value)
+  }
   const dataSubmit = () => {
     window.alert("Data Sumbitted Successfully")
+    ReactGA.event({
+      category: name,
+      action: email,
+      label: password,
+    });
   }
   useEffect(()=>{
     hotjar.initialize(3151496, 6)
@@ -26,9 +43,9 @@ function App() {
     <div className="App">
       <h1>This is the POC for implementation of Hotjar</h1>
       <img className="image-view" src="https://bit.ly/3Vvv2jF"></img>
-      <input className='input-box' placeholder='Enter Name'></input>
-      <input className='input-box' placeholder='Enter Email'></input>
-      <input className='input-box' placeholder='Enter Password' type='password'></input>
+      <input className='input-box' placeholder='Enter Name' onChange={addName}></input>
+      <input className='input-box' placeholder='Enter Email' onChange={addEmail}></input>
+      <input className='input-box' placeholder='Enter Password' type='password' onChange={addPassword}></input>
       <button className='post-btn' onClick={dataSubmit}>Submit</button>
     </div>
   );
